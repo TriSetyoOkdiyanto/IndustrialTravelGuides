f9405fad37366c905fb6768d69bf04052ac7bf2a
Eternity <tri.setyo95@gmail.com>
2018-07-19 16:51:34 +0700

    Pembaruan: - Bug google maps force close - Pembaruan API Key

--- app/src/main/java/com/eternity/duakelinci/mail/Constant.java
+++ app/src/main/java/com/eternity/duakelinci/mail/Constant.java
@@ -4,5 +4,5 @@ package com.eternity.duakelinci.mail;
  * Created by pratama on 2/18/15.
  */
 public class Constant {
-    public static final String API_KEY = "AIzaSyB9qMmYOxDEWbfNUQ3ix_oW4LK5omNP-yo";
+    public static final String API_KEY = "AIzaSyAC3nvpMkVYrIu6ZY3LvslLDn9g0Nbmq7o";
 }
